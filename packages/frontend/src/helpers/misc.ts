9ba6fd9961c4aadf4c7bfb83b5c7f37f07f0cb36
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-02 22:14:40 +0700

    (feature) Added an import alias for the src folder to the frontend project. Introduced reactRender function to clean up the main code entry for the frontend projet. Updated dependencies.

---
+++ packages/frontend/src/helpers/misc.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+import ReactDOM from "react-dom/client";
+
+export const reactRender = (component: ReactNode) => {
+  return ReactDOM.createRoot(document.getElementById("root")!).render(
+    component
+  );
+};
