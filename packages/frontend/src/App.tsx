58358523506e2d7dde8b17bb6d6584c747339c3c
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-05 22:44:19 +0700

    frontend: Added animated background.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import { ConnectButton } from '@mysten/dapp-kit'
 import '@mysten/dapp-kit/dist/index.css'
 import { FC } from 'react'
+import AnimatedBackground from './components/AnimatedBackground'
 
 const App: FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <ConnectButton />
-    </div>
+    <>
+      <main className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <ConnectButton />
+        </div>
+      </main>
+      <AnimatedBackground />
+    </>
   )
 }
 
