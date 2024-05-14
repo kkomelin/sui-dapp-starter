4e01d6e64bc9fda8fa16cc1dc925ff7f9f991d51
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-14 15:33:01 +0700

    frontend: Changed the logo.

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from '../assets/logo.svg'
 const Header = () => {
   return (
     <header className="flex flex-col items-center gap-3">
-      <img src={Logo} alt="Logo" className="h-16 w-16" />
+      <img src={Logo} alt="Logo" className="h-20 w-20" />
       <div className="text-2xl sm:text-3xl">
         {import.meta.env.VITE_APP_NAME}
       </div>
