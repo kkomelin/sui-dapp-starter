d2ffc395903c31ae4830e51f53058d86bf41545f
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-15 16:12:53 +0700

    backend/frontend: Made it possilbe to pass the network type to copy-package-id script.

--- packages/backend/scripts/copy-package-id.js
+++ packages/backend/scripts/copy-package-id.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 /**
- * The script copies the deployed package ID from the localnet logs to the frontend package as .env.local
+ * The script copies the deployed package ID from the corresponding Suibase network file to .env.local of the frontend package,
  * which is then read by the app.
+ * 
+ * The default network is localnet. To change it, pass "-n [NETWORK_TYPE]" through console.
  */
 
 const { promises } = require("node:fs");
@@ -10,31 +12,50 @@ const { homedir } = require("node:os");
 const path = require("node:path");
 const EnvFileWriter = require("env-file-rw").default;
 
-const NETWORK_TYPE = "localnet";
 const DEPLOYED_MODULE_NAME = "greeting";
 
-const SOURCE_FILE = path.join(
-  homedir(),
-  `/suibase/workdirs/${NETWORK_TYPE}/published-data/${DEPLOYED_MODULE_NAME}/most-recent/package-id.json`
-);
-
-const TARGET_FILE = path.join(process.cwd(), "../frontend/.env.local");
-
 const main = async () => {
+  const network = getNetworkFromArgs();
+  const sourceFile = sourceFilePath(network, DEPLOYED_MODULE_NAME);
+  const targetFile = targetFilePath();
+
   // Read SuiBase packageId file.
-  const data = await promises.readFile(SOURCE_FILE, "utf8");
+  const data = await promises.readFile(sourceFile, "utf8");
   const packageId = JSON.parse(data)[0];
 
   // Create .env.local file if it doesn't exist.
   try {
-    await promises.writeFile(TARGET_FILE, "", { flag: "wx" });
+    await promises.writeFile(targetFile, "", { flag: "wx" });
   } catch {}
 
   // Add VITE_CONTRACT_PACKAGE_ID variable to .env.local or update its value if it exists.
-  const envFileWriter = new EnvFileWriter(TARGET_FILE, false);
+  const envFileWriter = new EnvFileWriter(targetFile, false);
   await envFileWriter.parse();
   envFileWriter.set("VITE_CONTRACT_PACKAGE_ID", packageId);
   await envFileWriter.save();
+};
+
+const sourceFilePath = (network, deployedModuleName) => {
+  return path.join(
+    homedir(),
+    `/suibase/workdirs/${network}/published-data/${deployedModuleName}/most-recent/package-id.json`
+  );
+};
+
+const targetFilePath = () => {
+  return path.join(process.cwd(), "../frontend/.env.local");
+};
+
+const getNetworkFromArgs = () => {
+  const arg = process.argv.slice(2);
+
+  switch (arg[0]) {
+    case "-n":
+      return arg[1];
+
+    default:
+      return "localnet";
+  }
 };
 
 main().catch((e) => {
