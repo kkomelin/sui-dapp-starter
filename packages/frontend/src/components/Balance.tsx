a3e5fcb0c7b744fda0442d9cdef459be05852ddc
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-17 16:34:59 +0700

    frontend: Improved styles of the Balance component.

--- packages/frontend/src/components/Balance.tsx
+++ packages/frontend/src/components/Balance.tsx
@@ -4,7 +4,11 @@ import { Badge } from '@radix-ui/themes'
 const Balance = () => {
   const { balance } = useBalance()
 
-  return <Badge>Balance: {balance ? `${balance} SUI` : 'N/A'}</Badge>
+  return (
+    <Badge variant="surface" color="green" className="rounded-lg px-3 py-1.5">
+      Balance: {balance ? `${balance} SUI` : 'N/A'}
+    </Badge>
+  )
 }
 
 export default Balance
