5aae636565cb9213177ea399f0c7dc4031d69fe7
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-21 15:46:50 +0700

    frontend: Added a message is no Sui wallets detected.

--- packages/frontend/src/components/CustomConnectButton.tsx
+++ packages/frontend/src/components/CustomConnectButton.tsx
@@ -1,9 +1,22 @@
 import { useConnectWallet, useWallets } from '@mysten/dapp-kit'
-import { Button } from '@radix-ui/themes'
+import { Button, Link } from '@radix-ui/themes'
+import { SUI_WALLET_URL } from '~~/config/misc'
 
 const CustomConnectButton = () => {
   const wallets = useWallets()
   const { mutate: connect } = useConnectWallet()
+
+  if (wallets != null && wallets.length === 0) {
+    return (
+      <div className="text-center">
+        No Sui Wallet plugin detected. Please{' '}
+        <Link href={SUI_WALLET_URL} target="_blank" rel="noopener noreferrer">
+          install it
+        </Link>{' '}
+        and reload the page.
+      </div>
+    )
+  }
 
   return (
     <ul className="text-center">
@@ -13,7 +26,7 @@ const CustomConnectButton = () => {
             variant="solid"
             size="4"
             onClick={() => {
-              connect({ wallet }, { onSuccess: () => console.log('connected') })
+              connect({ wallet })
             }}
           >
             Connect to {wallet.name}
---
+++ packages/frontend/src/config/misc.ts
@@ -0,0 +1,2 @@
+export const SUI_WALLET_URL =
+  'https://chromewebstore.google.com/detail/sui-wallet/opcgpfmipidbgpenhmajoajpbobppdil'
