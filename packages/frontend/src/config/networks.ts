de7616dbe20747c527dfd980e11dcd44ec44573b
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-21 08:28:43 +0700

    backend: Added devnet and testnet commands.

--- packages/frontend/src/config/networks.ts
+++ packages/frontend/src/config/networks.ts
@@ -1,12 +1,17 @@
 // We automatically create/update .env.local with the deployed package ID after deployment.
+export const CONTRACT_PACKAGE_ID_NOT_DEFINED = '0xNOTDEFINED'
 export const LOCALNET_CONTRACT_PACKAGE_ID =
-  import.meta.env.VITE_LOCALNET_CONTRACT_PACKAGE_ID || '0xNOTDEFINED'
+  import.meta.env.VITE_LOCALNET_CONTRACT_PACKAGE_ID ||
+  CONTRACT_PACKAGE_ID_NOT_DEFINED
 export const DEVNET_CONTRACT_PACKAGE_ID =
-  import.meta.env.VITE_DEVNET_CONTRACT_PACKAGE_ID || '0xNOTDEFINED'
+  import.meta.env.VITE_DEVNET_CONTRACT_PACKAGE_ID ||
+  CONTRACT_PACKAGE_ID_NOT_DEFINED
 export const TESTNET_CONTRACT_PACKAGE_ID =
-  import.meta.env.VITE_TESTNET_CONTRACT_PACKAGE_ID || '0xNOTDEFINED'
+  import.meta.env.VITE_TESTNET_CONTRACT_PACKAGE_ID ||
+  CONTRACT_PACKAGE_ID_NOT_DEFINED
 export const MAINNET_CONTRACT_PACKAGE_ID =
-  import.meta.env.VITE_MAINNET_CONTRACT_PACKAGE_ID || '0xNOTDEFINED'
+  import.meta.env.VITE_MAINNET_CONTRACT_PACKAGE_ID ||
+  CONTRACT_PACKAGE_ID_NOT_DEFINED
 
 export const LOCALNET_EXPLORER_URL = 'http://localhost:9001'
 export const DEVNET_EXPLORER_URL = 'https://devnet.suivision.xyz'
