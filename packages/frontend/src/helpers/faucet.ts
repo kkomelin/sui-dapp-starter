37fda14b1746dbd6e071bfa5c3a8ffefc1a15fea
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-05-24 16:57:48 +0700

    frontend: Used newer API to fund an address through faucet.

--- packages/frontend/src/helpers/faucet.ts
+++ packages/frontend/src/helpers/faucet.ts
@@ -1,11 +1,11 @@
-import { getFaucetHost, requestSuiFromFaucetV0 } from '@mysten/sui.js/faucet'
+import { getFaucetHost, requestSuiFromFaucetV1 } from '@mysten/sui.js/faucet'
 import { ENetworksWithFaucet } from '~~/types/ENetworksWithFaucet'
 
 export const fundAddress = async (
   address: string,
   network: ENetworksWithFaucet
 ) => {
-  return await requestSuiFromFaucetV0({
+  return await requestSuiFromFaucetV1({
     host: getFaucetHost(network),
     recipient: address,
   })
