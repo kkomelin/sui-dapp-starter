09fd5ba54f6410f843af5732e678c3d277f1a57a
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-06-20 09:37:19 +0200

    frontend: Improved styles of the NetworkSupportChecker component.

--- packages/frontend/src/components/NetworkSupportChecker.tsx
+++ packages/frontend/src/components/NetworkSupportChecker.tsx
@@ -17,13 +17,15 @@ const NetworkSupportChecker = () => {
   }
 
   return (
-    <div className="m-2 mx-auto w-full max-w-lg rounded border border-red-400 px-3 py-2 text-center text-red-400">
-      The <span className="font-bold">{networkType}</span> is not currently
-      supported by the app.
-      <br />
-      Please switch to a supported network [
-      <span className="font-bold">{okNetworks.join(', ')}</span>] in your wallet
-      settings.
+    <div className="mx-auto w-full max-w-lg px-3 py-2">
+      <div className="w-full rounded border border-red-400 px-3 py-2 text-center text-red-400">
+        The <span className="font-bold">{networkType}</span> is not currently
+        supported by the app.
+        <br />
+        Please switch to a supported network [
+        <span className="font-bold">{okNetworks.join(', ')}</span>] in your
+        wallet settings.
+      </div>
     </div>
   )
 }
