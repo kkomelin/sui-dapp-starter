94aeae770837def5b3c2826c0e74afa470d1ecf6
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-07-17 12:40:17 +0200

    frontend: Added Arweave deployment command.

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,8 +3,11 @@ import path from 'path'
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const ARWEAVE_DEPLOYMENT = process.env.ARWEAVE_DEPLOYMENT === 'true' || false
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: !ARWEAVE_DEPLOYMENT ? '/' : './',
   resolve: {
     alias: {
       '~~': path.resolve(__dirname, './src'),
