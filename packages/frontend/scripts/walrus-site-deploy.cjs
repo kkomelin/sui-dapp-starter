3962d7e95693b5b762a205ec0f931301afb2bbc0
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-08-25 15:55:04 +0200

    frontend/infra: Developed a separate script for Walrus Sites deployment which either publish or update the app on Walrus Sites.

---
+++ packages/frontend/scripts/walrus-site-deploy.cjs
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+/**
+ * The script publishes to or updates the app on Walrus Sites.
+ * After publishing the app, site object ID is copied to .env.local, which is used later to update the app on Walrus Sites.
+ */
+
+const { promises } = require('node:fs')
+const path = require('node:path')
+const EnvFileWriter = require('env-file-rw').default
+const { execSync, exec } = require('node:child_process')
+
+const WALRUS_SITE_OBJECT_ID_VARIABLE_NAME = 'WALRUS_SITE_OBJECT_ID'
+
+const CONFIG_FILE_PATH = './.env.local'
+const WALRUS_SITES_CONFIG_PATH = './walrus-sites.yaml'
+const SITE_PATH = './dist'
+const WALLET_CONFIG_PATH_FULL = '~/suibase/workdirs/testnet/config/client.yaml'
+
+const main = async () => {
+  const configFilePathFull = path.join(process.cwd(), CONFIG_FILE_PATH)
+  const walrusConfigPathFull = path.join(
+    process.cwd(),
+    WALRUS_SITES_CONFIG_PATH
+  )
+  const sitePathFull = path.join(process.cwd(), SITE_PATH)
+
+  await createFileIfNecessary(configFilePathFull)
+
+  let siteObjectId = await readSiteObjectId(configFilePathFull)
+
+  // If the site has not yet been published (no site object ID in the config),
+  // then publish the site to Walrus Sites.
+  if (siteObjectId == null) {
+    const { stdout, stderr } = await exec(
+      `site-builder --config ${walrusConfigPathFull} --wallet ${WALLET_CONFIG_PATH_FULL} publish ${sitePathFull}`
+    )
+
+    // Get the site object ID from the publish command output.
+    stdout.on('data', async (data) => {
+      console.log(data)
+
+      const regex = /New site object ID: (.+)/
+      const result = data.match(regex)
+      if (result == null) {
+        return
+      }
+
+      siteObjectId = result[1].trim()
+
+      // if the publish command output doesn't contain site object ID, then report the error.
+      if (siteObjectId == null) {
+        // @todo: Handle error.
+        console.error('~ Could not find the site object ID in the output.')
+        console.error(
+          '~ If no errors occurred, please add WALRUS_SITE_OBJECT_ID=[site object ID from the output] to packages/frontend/.env.local manually.'
+        )
+        return
+      }
+
+      // Save site object ID to the config file.
+      await setEnvVar(
+        configFilePathFull,
+        WALRUS_SITE_OBJECT_ID_VARIABLE_NAME,
+        siteObjectId
+      )
+    })
+
+    stderr.on('data', async (error) => {
+      console.error(error)
+    })
+
+    return
+  }
+
+  // Update the site on Walrus Sites.
+  execSync(
+    `site-builder --config ${walrusConfigPathFull} --wallet ${WALLET_CONFIG_PATH_FULL} update ${sitePathFull} ${siteObjectId}`,
+    { stdio: 'inherit' }
+  )
+}
+
+/**
+ * Read Walrus site object ID from .env.local.
+ *
+ * @param {string} configFilePath
+ * @returns
+ */
+const readSiteObjectId = async (configFilePath) => {
+  const envFileWriter = new EnvFileWriter(configFilePath, false)
+  await envFileWriter.parse()
+  return envFileWriter.get(WALRUS_SITE_OBJECT_ID_VARIABLE_NAME, null)
+}
+
+/**
+ * Create a file if it doesn't exist.
+ *
+ * @param {string} filePath
+ * @returns
+ */
+const createFileIfNecessary = async (filePath) => {
+  try {
+    await promises.writeFile(filePath, '', { flag: 'wx' })
+  } catch {}
+}
+
+/**
+ * Set the environment variable in the .env.local file.
+ *
+ * @param {string} envFilePath
+ * @param {string} name
+ * @param {string} value
+ * @returns
+ */
+const setEnvVar = async (envFilePath, name, value) => {
+  const envFileWriter = new EnvFileWriter(envFilePath, false)
+  await envFileWriter.parse()
+  envFileWriter.set(name, value)
+  await envFileWriter.save()
+}
+
+// Main entry point.
+main().catch((e) => {
+  console.error(e)
+})
